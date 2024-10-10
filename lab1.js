64a498e744b24a78472c2c36926dc3c80aa2897d
Katrincik <grebennicova.ecaterina@gmail.com>
2024-10-11 00:00:27 +0300

    3task - extracted names and prices of the products

--- lab1.js
+++ lab1.js
@@ -1,14 +1,30 @@
 const url = 'https://carturesti.md/carte/the-cruel-prince-32197?p=13';
 
 fetch(url).then(response => {
-        // Check OK for status code 200-299
-        if (response.ok) {
-            return response.text();
-        } else {
-            throw new Error('Network response was not ok: ' + response.status);
-        }
-    }).then(htmlContent => {
-        console.log('HTML content:', htmlContent);
-    }).catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
+    if (response.ok) {
+        return response.text();
+    } else {
+        throw new Error('Network response was not ok: ' + response.status);
+    }
+}).then(htmlContent => {
+    const parser = new DOMParser();
+    const document = parser.parseFromString(htmlContent, 'text/html');
+
+    const bookName = document.querySelectorAll('.md-title');
+    const bookPrice = document.querySelectorAll('.suma');
+
+    let products = [];
+
+    bookName.forEach((name, index) => {
+        let product = {
+            name: name.textContent.trim(),
+            price: bookPrice[index] ? bookPrice[index].textContent.trim() : 'N/A'
+        };
+        products.push(product);
     });
+
+    console.log('Extracted data: ', products);
+
+}).catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+});

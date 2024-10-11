94e08f293215e90306f9ea95515412f57f285759
Katrincik <grebennicova.ecaterina@gmail.com>
2024-10-11 17:09:35 +0300

    Added some comments

--- lab1.js
+++ lab1.js
@@ -1,7 +1,10 @@
 const url = 'https://carturesti.md/carte/the-cruel-prince-32197?p=13';
 
+// Use fetch function for network request
 fetch(url).then(response => {
+    // If successful HTTP status code (200-299)
     if (response.ok) {
+        // Return HTML content as a string
         return response.text();
     } else {
         throw new Error('Network response was not ok: ' + response.status);
@@ -10,10 +13,12 @@ fetch(url).then(response => {
     const parser = new DOMParser();
     const document = parser.parseFromString(htmlContent, 'text/html');
 
+    // Select elements with matched CSS selectors
     const bookName = document.querySelectorAll('.md-title');
     const bookPrice = document.querySelectorAll('.suma');
     const mainBookAuthor = document.querySelector('.autorProdus');
 
+    // Array in order to hold info about books
     let products = [];
 
     bookName.forEach((name, index) => {
@@ -37,7 +42,7 @@ fetch(url).then(response => {
     const MDL_TO_EUR = 1 / EUR_TO_MDL;
 
     let convertBooks = products.map(product => {
-        let convertedPrice = (product.price * MDL_TO_EUR).toFixed(2);
+        let convertedPrice = (product.price * MDL_TO_EUR).toFixed(3);
 
         return {
             name: product.name,
